48be1016771ffc99071c251dabe261a8e99f62be
tanujas <tanujas@pharmarack.com>
2024-03-28 17:35:12 +0530

    counter added

---
+++ xstopwatch/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from  'react';
+
+function App() {
+  const [timer, setTimer] = useState(0);
+  const [isRunning,setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timerId;
+    if(isRunning) {
+      timerId = setInterval(() => { setTimer((prevTimer) => prevTimer+1) },1000);
+    }
+    return () => { clearInterval(timerId) };
+  },[isRunning, timer]);
+
+  const formatTime = (secs) => {
+    const mins = Math.floor(secs/60);
+    let remainingSecs = secs %  60;
+    return `${mins}:${remainingSecs < 10 ? "0" : ""}${remainingSecs}`;
+  }
+
+  const startStop = () => {
+    setIsRunning(!isRunning);
+  }
+
+  const reset = () => {
+    setTimer(0);
+    setIsRunning(false)
+  }
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <p>Time : {formatTime(timer)}</p>
+      <button onClick={startStop}>{ isRunning ? "Stop" : "Start" }</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
